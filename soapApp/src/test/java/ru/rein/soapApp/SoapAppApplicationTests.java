ebcb1a5814adf327f82bf07f1da6c3d1c05e2785
Rein <reinwlad@mail.ru>
2024-07-16 18:18:02 +0300

    Создал проект для SOAP приложения.

---
+++ soapApp/src/test/java/ru/rein/soapApp/SoapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rein.soapApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
