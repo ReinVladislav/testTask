a01e2cea6dade18b2af188e37d76e79250e48dc9
Rein <reinwlad@mail.ru>
2024-07-17 17:10:10 +0300

    вытащил <person> из Soap ответа.

--- restApp/src/main/java/ru/rein/restApp/services/PersonService.java
+++ restApp/src/main/java/ru/rein/restApp/services/PersonService.java
@@ -15,6 +15,7 @@ import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -28,6 +29,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -87,6 +89,9 @@ public class PersonService {
             HttpResponse response = httpClient.execute(httpPost);
             HttpEntity responseEntity = response.getEntity();
 
+
+
+
             if (responseEntity != null) {
 
                 String contentType = responseEntity.getContentType().getValue();
@@ -95,14 +100,40 @@ public class PersonService {
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(responseEntity.getContent(), StandardCharsets.UTF_8))) {
                     String responseString = reader.lines().collect(Collectors.joining("\n"));
                     System.out.println("Response: " + responseString);
-                    return responseString;
+
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    InputSource is = new InputSource(new StringReader(responseString));
+                    Document document = builder.parse(is);
+                    return getElementXml(document.getDocumentElement(), "person");
                 }
             }
         }
 
         return null;
     }
+    private static String getElementXml(Element parentElement, String elementTagName) {
+        NodeList nodeList = parentElement.getElementsByTagName(elementTagName);
+        if (nodeList.getLength() > 0) {
+            Element element = (Element) nodeList.item(0);
+            return elementToString(element);
+        }
+        return null;
+    }
 
+    // Преобразование элемента в строку XML
+    private static String elementToString(Element element) {
+        StringWriter sw = new StringWriter();
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.transform(new DOMSource(element), new StreamResult(sw));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
 
     private String jsonToXml(PersonDto personDto){
         JSONObject jsonObject = new JSONObject(personDto);
